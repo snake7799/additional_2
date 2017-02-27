cd3ed25cb34579d50cd31f83e1eab80402ef239c
Pavel Egorov <snake7799@users.noreply.github.com>
2017-02-27 22:31:41 +0300

    flatten/feature: implement flatten method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-module.exports = function flatten(array) {
-  // your solution
+module.exports = function flatten(array) { 
+    let result = [];
+    
+    if (!array || !Array.isArray(array))
+        return [];
+    
+    for (let key in array) 
+        if (array[key] instanceof Array)
+            Array.prototype.push.apply(result, flatten(array[key]));
+        else
+            result.push(array[key]);
+    
+    return result;
+   
+    
+    // Another way:
+    /*if (!array || !Array.isArray(array))
+        return [];
+    return array.reduce(function(previous, current) {
+        if (Array.isArray(current))
+            return previous.concat(flatten(current));
+        return previous.concat(current);
+    }, []);*/
 }
